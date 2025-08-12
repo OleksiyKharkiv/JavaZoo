a0d5f0e6bbd897eeb41b8366b013f300d15c6273
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-08-12 12:54:45 +0200

    Device 1.0

---
+++ src/Device.java
@@ -0,0 +1,38 @@
+public class Device {
+    String deviceName;
+    boolean isOn;
+    int power;
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public void turnOn() {
+        this.isOn = true;
+    }
+
+    public void turnOff() {
+        this.isOn = false;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+    Device lamp = new Device();
+    Device conditioner = new Device();
+    lamp.setPower(100);
+    conditioner.setPower(1500);
+
+}
