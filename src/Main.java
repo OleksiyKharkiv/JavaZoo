248aaf58a7a21a2e40508bfee9bc9ea51cda44e2
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-08-12 15:48:50 +0200

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        PowerGrid grid = new PowerGrid();
         Device lamp = new Device("Lamp", 100);
         Device conditioner1 = new Device("Conditioner", 1500);
         Device conditioner2 = new Device("Conditioner", 1500);
@@ -13,14 +13,43 @@ public class Main {
         conditioner2.turnOn();
         kettle.turnOn();
 
-        List<Device> devList = new ArrayList<>();
-        devList.add(lamp);
-        devList.add(conditioner1);
-        devList.add(conditioner2);
-        devList.add(kettle);
+        grid.addDevice(lamp);
+        grid.addDevice(conditioner1);
+        grid.addDevice(conditioner2);
+        grid.addDevice(kettle);
 
-        for (Device dev : devList) {
-            System.out.println(dev.getDeviceName() + " is " + (dev.getIsOn() ? "on" : "off"));
+        System.out.println("======= The System started ======= ");
+
+        printStatus(grid, kettle, conditioner2, conditioner2, lamp);
+
+        Random rand = new Random();
+        for (int i = 1; i < 6; i++) {
+            Thread.sleep(5000);
+            if (rand.nextDouble() < 0.3) {
+                System.out.println("\n[Датчик] Обнаружен перегрев чайника!");
+                kettle.updateSafety(false); // Помечаем как небезопасный → выключится автоматически
+            }
+
+            // Проверка безопасности сети (автоматический вызов при перегрузке)
+            grid.checkSafety();
+            System.out.printf("\n=== Состояние через %d сек. ===\n", i * 5);
+            printStatus(grid, kettle, conditioner1, conditioner2, lamp);
+
         }
+    }
+
+    private static void printStatus(PowerGrid grid, Device... devices) {
+        for (Device device : devices) {
+            String status = device.getIsOn() ? "ON" : "OFF";
+            String safety = device.updateSafety(true) ? "SAFE" : "DANGEROUS";
+            System.out.printf("[%s]: %s | %s | Потребление: %d Вт\n",
+                    device.getDeviceName(),
+                    status,
+                    safety,
+                    device.getCurrPower()
+            );
+        }
+        System.out.println("Total consumed power: = " + grid.getTotalPower() + "W");
+        System.out.println("======================================== ");
     }
 }
