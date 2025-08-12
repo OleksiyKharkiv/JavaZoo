1bd85c42e0bf6e666b76c7be8a3523a53491864d
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-08-12 14:26:49 +0200

    Add Sensor.java

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,2 +1,28 @@
 public class Sensor {
+    private int id;
+    private int deviceId;
+    private String sensorName;
+    private String sensorType;
+    public Sensor(int deviceId, String sensorName, String sensorType) {
+        this.deviceId = deviceId;
+        this.sensorName = sensorName;
+        this.sensorType = sensorType;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getDeviceId() {
+        return deviceId;
+    }
+    public String getSensorName() {
+        return sensorName;
+    }
+    public String getSensorType() {
+        return sensorType;
+    }
+    public boolean checkSafety(){
+        return true;
+    }
+
 }
