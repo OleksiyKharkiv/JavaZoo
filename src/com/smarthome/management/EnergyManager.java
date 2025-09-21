40e519e86cd59f0173b56d91d51755f43f7a4a85
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-09-21 19:04:37 +0200

    Add EnergyManager.java

--- src/com/smarthome/management/EnergyManager.java
+++ src/com/smarthome/management/EnergyManager.java
@@ -8,17 +8,31 @@ public class EnergyManager {
     private final List<Device> devices;
     private final int maxGridPower;
 
-    public EnergyManager (List<Device> devices, int maxGridPower){
+    public EnergyManager(List<Device> devices, int maxGridPower) {
         this.devices = devices;
         this.maxGridPower = maxGridPower;
     }
+
     public int getCurrTotalPowerConsumption() {
         return devices.stream()
                 .mapToInt(Device::getCurrPowerConsumption)
                 .sum();
     }
+
     public boolean isGridOverloaded() {
         return getCurrTotalPowerConsumption() > maxGridPower;
     }
 
+    public void autiDiableDevices() {
+        Device maxDevice = null;
+        for (Device device : devices) {
+            if (device.isOn() && (maxDevice == null || device.getCurrPowerConsumption() > maxDevice.getCurrPowerConsumption())
+            {
+                maxDevice = device;
+            }
+        }
+        if (maxDevice != null) {
+            maxDevice.turnOff();
+        }
+    }
 }
