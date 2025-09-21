32b3f45a2580c8648e0b7d7442c0d1eb66ccb4e4
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-09-21 17:25:09 +0200

    Add SmartOutlet.java to devices

---
+++ src/main/com/smarthome/devices/SmartOutlet.java
@@ -0,0 +1,20 @@
+package main.com.smarthome.devices;
+
+public class SmartOutlet extends AbstractDevice {
+    private boolean isOverloaded;
+
+    public SmartOutlet(String id, String name, boolean isOn, int powerConsumption) {
+        super(id, name, isOn, powerConsumption);
+        this.isOverloaded = false;
+    }
+
+    public String toString() {
+        return "SmartOutlet{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", isOn=" + isOn +
+                ", powerConsumption=" + powerConsumption +
+                ", isOverloaded=" + isOverloaded +
+                '}';
+    }
+}
