667071f9cc08e74c0496cff8b12f2efb4ca2a891
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-09-21 18:37:41 +0200

    Add DeviceFactory.java

--- src/com/smarthome/factory/DeviceFactory.java
+++ src/com/smarthome/factory/DeviceFactory.java
@@ -1,5 +1,18 @@
 package com.smarthome.factory;
 
+import com.smarthome.devices.SmartLight;
+import com.smarthome.devices.SmartOutlet;
+
 public class DeviceFactory {
+    public SmartLight createSmartLight(String name, boolean isOn, int powerConsumption ) {
+        return new SmartLight(generateId(), name, isOn, powerConsumption);
+    }
+    public SmartOutlet createSmartOutlet(String name, boolean isOn, int powerConsumption ) {
+        return new SmartOutlet(generateId(), name, isOn, powerConsumption);
+    }
+
+    private static String generateId() {
+        return "dev_" + System.currentTimeMillis() + "_" + (int)(Math.random() * 1000);
+    }
 
 }
