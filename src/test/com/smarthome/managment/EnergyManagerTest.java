2d7f68eaf5f2c9713d939a66426898a00ab0a382
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-10-07 20:08:23 +0200

    Fix EnergyManagerTest.java

--- src/test/com/smarthome/managment/EnergyManagerTest.java
+++ src/test/com/smarthome/managment/EnergyManagerTest.java
@@ -3,12 +3,16 @@ package test.com.smarthome.managment;
 import com.smarthome.core.Device;
 import com.smarthome.devices.SmartOutlet;
 import com.smarthome.management.EnergyManager;
+import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class EnergyManagerTest {
+    @Test
     void shouldDisableDevicesWhenGridOverloaded() {
         //Given
         List<Device> devices = Arrays.asList(
@@ -16,20 +20,14 @@ public class EnergyManagerTest {
                 new SmartOutlet("outlet2", "Outlet2", true, 1500)
         );
         EnergyManager energyManager = new EnergyManager(devices, 1200);
-        
+
         //when
         devices.forEach(Device::turnOn);
         energyManager.autoDiableDevices();
-        
+
         //then
         assertFalse(energyManager.isGridOverloaded());
         assertEquals(500, energyManager.getCurrTotalPowerConsumption());
     }
 
-    private void assertEquals(int i, int currTotalPowerConsumption) {
-    }
-
-    private void assertFalse(boolean gridOverloaded) {
-        
-    }
 }
